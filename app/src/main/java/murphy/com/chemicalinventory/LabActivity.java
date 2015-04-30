839c2c4e5dd82e3e21838d2c431dc37fa2384194
Christian Murphy <Christian.Murphy.42@gmail.com>
2015-04-30 11:35:48 -0700

    add create lab database connection

--- app/src/main/java/murphy/com/chemicalinventory/LabActivity.java
+++ app/src/main/java/murphy/com/chemicalinventory/LabActivity.java
@@ -4,6 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import io.realm.Realm;
+import murphy.com.chemicalinventory.models.Lab;
 
 
 public class LabActivity extends AppCompatActivity {
@@ -12,6 +17,25 @@ public class LabActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lab);
+    }
+
+    public void createLab(View view) {
+        EditText labNameEdit = (EditText) findViewById(R.id.edit_lab_name);
+        EditText labLocationEdit = (EditText) findViewById(R.id.edit_lab_location);
+        EditText labManagerEdit = (EditText) findViewById(R.id.edit_lab_manager);
+
+        String labName = labNameEdit.getText().toString();
+        String labLocation = labLocationEdit.getText().toString();
+        String labManager = labManagerEdit.getText().toString();
+
+        Realm realm = Realm.getInstance(this);
+
+        realm.beginTransaction();
+        Lab lab = realm.createObject(Lab.class); // Create a new object
+        lab.setName(labName);
+        lab.setLocation(labLocation);
+        lab.setManager(labManager);
+        realm.commitTransaction();
     }
 
 
