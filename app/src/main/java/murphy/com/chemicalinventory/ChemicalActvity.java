ea79818aa93d77a196eacd081962b7064ad434cd
Christian Murphy <Christian.Murphy.42@gmail.com>
2015-05-01 19:02:57 -0700

    all the views are connected

--- app/src/main/java/murphy/com/chemicalinventory/ChemicalActvity.java
+++ app/src/main/java/murphy/com/chemicalinventory/ChemicalActvity.java
@@ -1,32 +1,24 @@
 package murphy.com.chemicalinventory;
 
-import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class ListChemicalsActivity extends AppCompatActivity {
+public class ChemicalActvity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d("List Chemicals", getIntent().getExtras().getString("name"));
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_chemicals);
+        setContentView(R.layout.activity_chemical_actvity);
     }
 
-    public void createChemical(View view) {
-        Intent createLabActivity = new Intent(this, ChemicalActvity.class);
-        startActivity(createLabActivity);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_list_chemicals, menu);
+        getMenuInflater().inflate(R.menu.menu_chemical_actvity, menu);
         return true;
     }
 
